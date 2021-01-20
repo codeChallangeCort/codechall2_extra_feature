9d3f7453b6f17c80544f0243cf22569895ec3436
cortCodeChall <cortcodechall@gmail.com>
2021-01-19 23:35:55 -0500

    Wrong Email and multiple fiels exception solved

--- start.py
+++ start.py
@@ -3,15 +3,16 @@ import json
 import sys
 import re
 
-
 # script would take args as filename.csv."
 
 try:
     parsed_files = []
-    for each_file in sys.argv[1:]:  
+    for each_file in sys.argv[1:]:
         parsed_files.append(each_file)
-    print("Total Files parsed: ", parsed_files)   
+    print("Total Files parsed: ", len(parsed_files), parsed_files)
+
 except Exception as e:
+    print(e)
     sys.exit()
 
 # Empty Dictionary for JSON dumps
@@ -24,50 +25,63 @@ json_data["user_list"] = []
 
 # list_id will start from 0
 
-list_id = 1
+list_id = 0
 
 # user_list_size is the count of customer 
 
 json_data["user_list_size"] = 0
 
+regex = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
 
-regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+email_list = []
 
 def getInfo(row):
     """
     This FUncation will split the full name in firstname and lastname, with list_id, and will dump that information in temporary dict
     and temp dict into a list of
     json_data
-    """ 
+    """
 
     # json_temp is to get the info for a individual customer for temporary time.
-    
+
     json_temp = {}
     global list_id
-    json_temp["list_id"] = list_id
-    
-    if " " in str(row["full_name"]) and re.search(regex,row["email"]):
-        full_name = row["full_name"].split(" ")
-        json_temp["first_name"] = full_name[0]
-        json_temp["last_name"] = full_name[1]
+
+    if re.search(regex,row["email"]):
+        if row["email"] not in email_list:
+
+            list_id += 1
+            json_temp["list_id"] = list_id
+            json_temp["email"] = row["email"]
+            if " " in str(row["full_name"]) and len(row["full_name"]) >= 3:
+                full_name = row["full_name"].split(" ")
+                json_temp["first_name"] = full_name[0]
+                json_temp["last_name"] = full_name[1]
+            else:
+                json_temp["first_name"] = "Customer"
+                json_temp["last_name"] = "N/A"
+            email_list.append(row["email"])
+            json_data["user_list"].append(json_temp)
+        else:
+            print("Duplicate found: ", row["full_name"], row["email"])
     else:
-        json_temp["first_name"] = row["full_name"]
-        json_temp["last_name"] = "N/A"
-    json_temp["email"] = row["email"]
+        print("Invalid Information: ", row["full_name"], row["email"])
 
-    # json_temp will be appended into the list of json_data.
 
-    json_data["user_list"].append(json_temp)
-    list_id += 1
+if __name__ == "__main__":
     
-# If more than one filesname has been passed with "," then for each file data would be readed and stored into json_data.
-    
-for parsed_file in parsed_files:
-    df = pd.read_csv(parsed_file)
-    json_data["user_list_size"] += len(df)
-    df.apply(lambda x: getInfo(x), axis=1)
+    missing_file = []
+    for parsed_file in parsed_files:
+        try:
+            df = pd.read_csv(parsed_file)
+            df.apply(lambda x: getInfo(x), axis=1)
 
-# To dump the json_data into result.json.
+        except Exception as e:
+            missing_file.append(parsed_file)
+        continue
 
-with open('results.json', 'w') as outfile:
-    json.dump(json_data, outfile, indent=4)
+    print("TOTAL MISSING FILES: ",len(missing_file), missing_file)
+    json_data["user_list_size"] = list_id
+
+    with open('results.json', 'w') as outfile:
+        json.dump(json_data, outfile, indent=4)
